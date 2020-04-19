3221902554c42acf04f4bb871edd763c02f65e67
LanaYer <e.svetlana@molinos.ru>
2020-04-19 16:19:17 +0300

    new project

---
+++ js/main.js
@@ -0,0 +1,34 @@
+﻿var min_id = 0;
+
+$( document ).ready(function() {
+
+    $('.east-header-hamburger').on('click',function () {
+
+        $( '.east-menu' ).slideToggle( "slow", function() {
+
+        });
+    });
+
+    $('.east-header-username').on('click',function () {
+
+        $( '.east-header-personal_menu' ).slideToggle( "slow", function() {
+
+        });
+    });
+
+
+
+    //---------------------------------------------------------------------------------------
+
+    /* 4. Calendar */
+    $('#calendar').datepicker({
+        dateFormat : "yy-mm-dd",
+        minDate: new Date($('#hiddendelivdate').val()),
+        monthNames : ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        dayNamesMin : ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+    });
+});
+
+//---------------------------------------------------------------
+
+	
